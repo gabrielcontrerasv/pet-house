5304e7d72e7635e0b54e557084a4337130b5feb1
gabriel contreras <gabriel.contrerasv3@gmail.com>
2022-08-18 00:01:51 -0500

    limpiando app.js

--- app.js
+++ app.js
@@ -29,21 +29,6 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-/*
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
-*/
-/*
-app.use(cors({
-  origin : "*"
-}))
-*/
 
 app.use(
   cors({
